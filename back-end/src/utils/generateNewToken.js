0bb2f2a56f56d0073782daa315feba2551f31588
Paulo Frazão <paulo4187@gmail.com>
2021-03-17 18:16:51 -0300

    fix: secret token

--- back-end/src/utils/generateNewToken.js
+++ back-end/src/utils/generateNewToken.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
 module.exports = async (email) => {
+const secret = 'testando';
+
   const jwtConfig = {
     expiresIn: '10d',
     algorithm: 'HS256',
   };
-  const token = await jwt.sign({ data: email }, process.env.TOKEN_SECRET, jwtConfig);
+  const token = await jwt.sign({ data: email }, secret, jwtConfig);
   return token;
 };
