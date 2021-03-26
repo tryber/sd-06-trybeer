83ac39606e59c6a9585a9a89a5eae7290cafa309
Paulo Frazão <paulo4187@gmail.com>
2021-03-26 15:09:21 -0300

    testando umas implementaçoes para estudoooooop15

--- back-end/src/controller/OrderController.js
+++ back-end/src/controller/OrderController.js
@@ -1,11 +1,12 @@
 const rescue = require('express-rescue');
 
 const { OrderService } = require('../service');
-const { OrderModel } = require('../model');
+const { OrderModel, ProductsModel } = require('../model');
 
 const getAllOrders = rescue(async (_req, res) => {
   const [orders] = await OrderModel.getAllOrders();
- 
+  const [prod] = await ProductsModel.getAllProducts();
+  console.log(prod);
 const saled = 'sale_date';
 const total = 'total_price';
 const lista = [{ id: 1, [saled]: '2021-03-26 11:28:46', [total]: '2.20' }];
