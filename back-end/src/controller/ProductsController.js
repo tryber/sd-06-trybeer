f3320d401eb64dcceac5e6e07fe31dc0a33271df
Paulo Frazão <paulo4187@gmail.com>
2021-03-26 13:39:23 -0300

    testando umas implementaçoes para estudoooooop16

--- back-end/src/controller/ProductsController.js
+++ back-end/src/controller/ProductsController.js
@@ -4,6 +4,7 @@ const { ProductsService } = require('../service');
 
 const listProducts = rescue(async (_req, res) => {
   const getAllProducts = await ProductsService.getAllProducts();
+console.log(getAllProducts);
   return res
     .status(200)
     .json(getAllProducts);
