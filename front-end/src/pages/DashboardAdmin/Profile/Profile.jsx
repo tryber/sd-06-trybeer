a09791c85e32a909dac6f1814a8a542296cce22d
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-03-26 07:57:03 -0300

    pegando a tela de perfil do adm

--- front-end/src/pages/DashboardAdmin/Profile/Profile.jsx
+++ front-end/src/pages/DashboardAdmin/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../../../components/Header/Header';
 import { verifyAdmin } from '../../../store/LocalStorage/actions';
+import './Profile.css';
 
 export default function Profile() {
   const [nome, setNome] = useState('');
@@ -15,11 +16,15 @@ export default function Profile() {
   }, [history]);
 
   return (
-    <div>
+    <div className="container">
       <Header title="TryBeer" user="admin" />
-      <h1>Perfil</h1>
-      <h4 data-testid="profile-name">{`Nome: ${nome}`}</h4>
-      <h4 data-testid="profile-email">{`Email: ${emailAdmin}`}</h4>
+      <div className="perfil">
+        <h1>Perfil</h1>
+      </div>
+      <div className="dados">
+        <h4 data-testid="profile-name">{`Nome: ${nome}`}</h4>
+        <h4 data-testid="profile-email">{`Email: ${emailAdmin}`}</h4>
+      </div>
     </div>
   );
 }
